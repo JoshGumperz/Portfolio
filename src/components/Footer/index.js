457b971c5d075079d26f8297a826304f1f9d24e3
Josh Gumperz <joshg3023@gmail.com>
2023-01-19 16:35:40 -0800

    made footer links open in new tab

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,9 +8,9 @@ function Footer() {
                 <div className="footer-body">
                     <h2 className="footer-h2">Contact Info</h2>
                     <div className='footer-linkContainer'>
-                        <a className="footer-link" href='mailto:joshgumperz@gmail.com'>joshgumperz@gmail.com</a>
-                        <a className="footer-link" href='https://github.com/JoshGumperz'>https://github.com/JoshGumperz</a>
-                        <a className="footer-link" href='https://www.linkedin.com/in/josh-gumperz-8706a8185/'>https://www.linkedin.com/in/josh-gumperz-8706a8185/</a>
+                        <a className="footer-link" href='mailto:joshgumperz@gmail.com' rel="noopener noreferrer" target="_blank">joshgumperz@gmail.com</a>
+                        <a className="footer-link" href='https://github.com/JoshGumperz' rel="noopener noreferrer" target="_blank">https://github.com/JoshGumperz</a>
+                        <a className="footer-link" href='https://www.linkedin.com/in/josh-gumperz-8706a8185/' rel="noopener noreferrer" target="_blank">https://www.linkedin.com/in/josh-gumperz-8706a8185/</a>
                     </div>
                 </div>
             </div>
